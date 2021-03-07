be528f144a0813b867ac61828a50a3f2b15432b6
TheSoloLeveling <62574566+TheSoloLeveling@users.noreply.github.com>
2021-03-07 11:46:18 +0100

    Add Movement/Animation Script

--- Source/MyProject3/MainCharacter.cpp
+++ Source/MyProject3/MainCharacter.cpp
@@ -2,6 +2,13 @@
 
 
 #include "MainCharacter.h"
+#include "Components/InputComponent.h"
+#include "GameFramework/Controller.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+#include "Engine/World.h"
+#include "GameFramework/PlayerController.h"
+
 
 // Sets default values
 AMainCharacter::AMainCharacter()
@@ -9,12 +16,26 @@ AMainCharacter::AMainCharacter()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>("SpringArmComponent");
+	CameraBoom->SetupAttachment(RootComponent);
+
+	Camera = CreateDefaultSubobject<UCameraComponent>("CameraComponent");
+	Camera->SetupAttachment(CameraBoom);
+
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
+
 }
 
 // Called when the game starts or when spawned
 void AMainCharacter::BeginPlay()
 {
 	Super::BeginPlay();
+
+	
+	APlayerController* PlayerController = Cast<APlayerController>(Controller);
+	
 	
 }
 
@@ -29,6 +50,39 @@ void AMainCharacter::Tick(float DeltaTime)
 void AMainCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
+	PlayerInputComponent->BindAxis("MoveForward", this, &AMainCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &AMainCharacter::MoveRight);
+	
 }
+
+void AMainCharacter::MoveForward(float AxisValue)
+{
+	const FRotator Rotation = Controller->GetControlRotation();
+	
+	const FRotator YawRotation(0, Rotation.Yaw, 0);
+	const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+
+	AddMovementInput(Direction, AxisValue);
+
+	
+
+
+	//AddMovementInput(GetActorForwardVector(), AxisValue * TurnRate);
+}
+
+void AMainCharacter::MoveRight(float AxisValue)
+{
+	const FRotator Rotation = Controller->GetControlRotation();
+	
+
+
+	const FRotator YawRotation(0, Rotation.Yaw, 0);
+	const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+	AddMovementInput(Direction, AxisValue);
+
+	//AddMovementInput(GetActorRightVector() , AxisValue * TurnRate);
+}
+
+
 
--- Source/MyProject3/MainCharacter.h
+++ Source/MyProject3/MainCharacter.h
@@ -15,6 +15,14 @@ public:
 	// Sets default values for this character's properties
 	AMainCharacter();
 
+private:
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Camera", meta = (AllowPrivateAccess = "true"))
+	class USpringArmComponent* CameraBoom;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Camera", meta = (AllowPrivateAccess = "true"))
+	class UCameraComponent* Camera;
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
@@ -25,5 +33,16 @@ public:
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UPROPERTY(EditAnywhere)
+	float TurnRate = 10;
+
+	UPROPERTY(EditAnywhere)
+	float RotationRate = 10;
+
+	void MoveForward(float AxisValue);
+	void MoveRight(float AxisValue);
+	
+
 
 };
